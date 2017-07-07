9cf912b8b3e37a57bee74c074a48220542d9a73b
Sergey <ror.amconsoft1@gmail.com>
2017-07-07 17:51:08 +0300

    testing & form requesting

--- first_express_app.js
+++ first_express_app.js
@@ -7,16 +7,49 @@ var element = require('./lib/element');
 app.engine('hbs', handlebars({ extname: 'hbs', defaultLayout: 'main.hbs' }));
 app.set('view engine', 'hbs');
 
+app.use(require('body-parser')());
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
+
+// check param for showing tests on page (if not in production env)
+// ex: localhost:3000?test=1 => show tests
+app.use(function (req, res, next) {
+  // res.locals - context that will be passed to views
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
 
 app.get('/', function (req, res) {
   res.render('home');
 });
 
 app.get('/about', function (req, res) {
-  res.render('about', { element: element.getElement() });
+  res.render('about', {
+    element: element.getElement(),
+    pageTestScript: '/qa/tests-about.js'
+  });
+});
+
+app.get('/tours/hood-river', function (req, res) {
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function (req, res) {
+  res.render('tours/request-group-rate');
+});
+
+app.get('/newsletter', function (req, res) {
+  res.render('newsletter', { csrf: 'CSRF token' });
+});
+
+app.post('/process', function (req, res) {
+  if(req.xhr || req.accepts('json,html') === 'json') {
+    res.send({ success: true });
+  } else {
+    res.redirect(303, '/thank-you');
+  }
 });
 
 // 404 catch-all handler (middleware)
